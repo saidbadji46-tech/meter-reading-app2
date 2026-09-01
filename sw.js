6ece5772813cc27ce81cfc8fed85009a19fff03a
saidbadji46-tech <saidbadji46@gmail.com>
2026-09-01 15:37:30 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - يخزن التطبيق للعمل بلا انترنت، ويسولك قبل ما يبدل لنسخة جديدة
-const CACHE_NAME = 'meter-reading-v5';
+const CACHE_NAME = 'meter-reading-v6';
 
 self.addEventListener('install', () => {
   /* ماندير-وش skipWaiting هنا، باش النسخة الجديدة تبقى "فحالة انتظار"
