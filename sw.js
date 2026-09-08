5ae7986a375c61abbd0a062f27e434c8aab4960c
saidbadji46-tech <saidbadji46@gmail.com>
2026-09-09 00:11:27 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'meter-reading-v22-pro';
+const CACHE_NAME = 'meter-reading-v24-pro';
 const APP_SHELL = [
   './', './index.html', './manifest.json', './icon-192x192.png', './icon-512x512.png',
   './vendor/jszip.min.js',
